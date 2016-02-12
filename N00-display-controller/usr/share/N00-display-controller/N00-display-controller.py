44528dfc5f331e4a287bd7597b89e633d93388dc
Xavier Raemy <Blup1980@users.noreply.github.com>
2016-02-12 15:55:40 +0100

    Added skeleton for remote access

---
+++ N00-display-controller/usr/share/N00-display-controller/N00-display-controller.py
@@ -0,0 +1,28 @@
+# N00-display-controller.
+# Copyright (C) 2016  Blup1980
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import daemon
+
+from N00_display import Display
+from sequencer import Sequencer
+
+
+dis = Display()
+seq = Sequencer('file://.planning.csv',1)
+
+while True:
+    dis.show(msg)
