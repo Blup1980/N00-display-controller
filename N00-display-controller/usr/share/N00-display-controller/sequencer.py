f566a948ff7aa71961e586f9429ded32c6be274d
Xavier Raemy <Blup1980@users.noreply.github.com>
2016-02-13 17:47:46 +0100

    Parsing of the remote CSV file sequencer done

--- N00-display-controller/usr/share/N00-display-controller/sequencer.py
+++ N00-display-controller/usr/share/N00-display-controller/sequencer.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from time import time
+from datetime import datetime
 import requests
 
 
@@ -26,7 +26,7 @@ class Sequencer:
         :param interval_min: the interval time to recheck for file changes in minutes
         :return:
         """
-        if ~isinstance(url, str):
+        if not isinstance(url, str):
             raise TypeError
 
         if interval_min <= 0:
@@ -43,10 +43,31 @@ class Sequencer:
         :return: string of the msg to display
         """
 
-        if self.last_check_time + self.interval <= time():
+        now = datetime.today()
+        if self.last_check_time + self.interval <= now.timestamp():
             self._refresh()
 
-    def _refresh(self):
-        r = requests.get(self.URL)
-        #TODO do the parsing
+        msg = ''
+        for t in self.sequence:
+            if t['start'] <= now < t['stop']:
+                msg = t['msg']
+        return msg
 
+    def _refresh(self):
+        # TODO manage the exception of no connexion or issues with the parsing
+        r = requests.get(self.URL)
+        if not r.ok:
+            return
+        lines = r.text.split('\n')
+        parsed = [l.split(';') for l in lines]
+        parsed = parsed[1:]
+        seq = []
+        for f in parsed:
+            try:
+                time_start = datetime.strptime(f[0], '%d.%m.%Y %H:%M')
+                time_stop = datetime.strptime(f[1], '%d.%m.%Y %H:%M')
+                msg = f[2]
+                seq.append({'start': time_start, 'stop': time_stop, 'msg': msg})
+            except ValueError:
+                pass
+        self.sequence = seq
