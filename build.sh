a0e84fd674dd30563c9cb888b375c5d59dac6d43
Xavier Raemy <Blup1980@users.noreply.github.com>
2016-02-16 10:48:32 +0100

    Revision of the packaging system

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-sudo dpkg-deb --build N00-display-controller
+mkdir -p build/usr/share/N00-display-controller/
+mkdir -p build/etc/
+mkdir -p build/DEBIAN/
+rsync --exclude .idea -aR ./src/ build/usr/share/N00-display-controller/
+rsync --exclude .idea -aR ./etc/ build/
+rsync -a debianScripts/ build/DEBIAN
+
+sudo dpkg-deb --build build N00-display-controller.deb
+
+rm -rf ./build
